e8aafa4d71e4d2f353cc1e7e64be12d9c42549a6
Linux Admin Hacks <123953494+linuxadminhacks@users.noreply.github.com>
2023-12-18 22:55:08 +0330

    Update show_current_user_info.sh

--- show_current_user_info.sh
+++ show_current_user_info.sh
@@ -7,17 +7,17 @@ echo "Current user: $(whoami)"
 echo "Total size of home directory: $(du -sh $HOME | cut -f1)"
 
 # Print the login time and duration
-current_time=$(date +'%H:%M')
+#current_time=$(date +'%H:%M')
 logged_at=$(who | grep "$(whoami)" | awk '{print $4}')
-current_timestanp=$(date -d "$current_time" +%s)
-logged_timestamp=$(date -d "$logged_at" +%s)
+#current_timestanp=$(date -d "$current_time" +%s)
+#logged_timestamp=$(date -d "$logged_at" +%s)
 
-difference=$((current_timestamp - logged_timestamp))
+#difference=$((current_timestamp - logged_timestamp))
 
-logged_time=$(date -u -d @$difference +"%H:%M:%S")
+#logged_time=$(date -u -d @$difference +"%H:%M:%S")
 
 echo "Logged in at: $logged_at"
-echo "Total login time: $logged_time"
+echo "Total login time: $(ps -o etime= -p $$) | grep -v ' '"
 
 # Print the last login time
 echo "Last login time: $(lastlog -u $(whoami) | awk '{print $4, $5, $6}' | grep -v "Latest")"
