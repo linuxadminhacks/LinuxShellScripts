ec38ffb823475dc76a1c0396f43c4d1f0355505d
Linux Admin Hacks <123953494+linuxadminhacks@users.noreply.github.com>
2023-12-18 18:45:57 +0330

    Create show_current_user_info.sh

---
+++ show_current_user_info.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Print the current user
+echo "Current user: $(whoami)"
+
+# Print the total size of the home directory
+echo "Total size of home directory: $(du -sh $HOME | cut -f1)"
+
+# Print the login time and duration
+echo "Logged in since: $(who -b | awk '{print $3, $4}')"
+echo "Time remaining: $(uptime | awk '{print $3, $4}')"
+
+# Print the last login time
+echo "Last login time: $(lastlog -u $(whoami) | awk '{print $4, $5, $6}')"
+
+# Print the last password change time
+echo "Last password change: $(chage -l $(whoami) | grep "Last password change" | awk -F: '{print $2}')"
