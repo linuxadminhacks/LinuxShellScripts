b2e50746e5f186fc17d823355d7014f0c2fe38e6
Linux Admin Hacks <123953494+linuxadminhacks@users.noreply.github.com>
2023-12-27 13:45:27 +0330

    Update check_if_user_login_on_specified_date.sh

--- check_if_user_login_on_specified_date.sh
+++ check_if_user_login_on_specified_date.sh
@@ -3,14 +3,24 @@
 username=$1
 target_date=$2
 
-# Check if the target date is in the correct format
-if [[ ! $target_date =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]
-then
- echo "Invalid date format. Please enter date in the format YYYY-MM-DD."
- exit 1
+if [[ $# -ne 2 ]]; then
+   echo "Username or Date not provided."
+   echo "Usage: $0 <username> <date>."
+   exit 1
 fi
 
-# Convert the date to the correct format if it is not already
+if ! id $username >/dev/null 2>&1; then
+   echo "User $username not found."
+   exit 1
+fi
+
+# Check if the target date is in the correct format
+if [[ $target_date =~ ^[0-9]+$ ]]; then
+ target_date="$target_date days ago"
+ echo "$target_date"
+ target_date=$(date -I -d "$target_date")
+fi
+
 target_date=$(date -I -d "$target_date")
 
 # Set the start and end dates to cover the entire day for the target date
@@ -18,7 +28,7 @@ start_date="${target_date}T00:00:00"
 end_date="${target_date}T23:59:59"
 
 # Check if the user logged in on the target date
-if last --time-format=iso -s $start_date -t $end_date | grep -q "^$username"; then
+if last --time-format=iso -s $start_date -t $end_date | grep -q  "$username"; then
  echo "User $username logged in on $target_date."
 else
  echo "User $username did not log in on $target_date."
